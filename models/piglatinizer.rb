27bd636884797e07e0e7abc05b3a674a5cf6078d
jlangley3 <jlangleyiii@aol.com>
2020-05-13 14:29:59 -0400

    last

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,27 @@
-class PigLatinizer
-    attr_reader :text
-    def initialize(text)
-        @text = text.downcase
+class PigLatinizer 
+    
+    def piglatinize(string)
+        string = string.to_s.downcase
+        string.split("")
+        if vowels,include?
     end
-    #take in the string, downcase
-    ##split into individual letters
-    #take everything before a vowel and put at end
-    #add ay if we remove beginning to end,
-    # except in case of vowels add (way), if noting taken off 
+     def vowel(letter)
+         letter.match(/[aeiou]/)
+     end
     def letters
-        letters = text.split("")
-        puts letters
+        letters = text
     end
+    # take in the string, downcase
+    #split into individual letters
+    # take everything before a vowel and put at end
+    # add ay if we remove beginning to end,
+    # except in case of vowels add (way), if noting taken off 
+    
 
 end
 
-PigLatinizer.new("string")
 
-"String".letters
+
+
+
 
