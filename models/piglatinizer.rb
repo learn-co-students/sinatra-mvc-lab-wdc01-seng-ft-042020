438eecb87d3a93b7c759503a55e173aa3a5b4f1a
jlangley3 <jlangleyiii@aol.com>
2020-05-13 13:01:14 -0400

    just starting

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,11 +1,21 @@
 class PigLatinizer
-
+    attr_reader :text
     def initialize(text)
         @text = text.downcase
     end
-
-    def word
-
+    #take in the string, downcase
+    ##split into individual letters
+    #take everything before a vowel and put at end
+    #add ay if we remove beginning to end,
+    # except in case of vowels add (way), if noting taken off 
+    def letters
+        letters = text.split("")
+        puts letters
     end
 
 end
+
+PigLatinizer.new("string")
+
+"String".letters
+
