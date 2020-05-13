234250b9244eded4443e6237661ed37c3e11e4e5
epittab <ep7m@me.com>
2020-05-13 15:19:45 -0400

    PigLatin class finished it returns a pig latin string on the #ultimate_method

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,9 +10,8 @@ class Piglatin
     # DONE get string into expression after initialization
     # DONE make sure that expression is an array of word(s)
     # DONE use an enumerable on the array of strings 
-    # each time through we can call a convert method
-    # create a convert method that takes in a string and creates a pig latin version of the string
-
+    # DONE each time through we can call a convert method
+    # DONE create a convert method that takes in a string and creates a pig latin version of the string
 
 
     def create_array_of_words
@@ -21,25 +20,32 @@ class Piglatin
     end
 
 
-    def convert
-        # we will use it like this some_instance.convert
+    def convert(word)
+     
+        if /^[^aeiou]+/i.match?(word)   #Regex.match? the string
+            ending = /^[^aeiou]+/i.match(word) 
+            ending = ending.to_s
+            ending += "ay"
+   
+            new_word = word.gsub(/^[^aeiou]+/i, "")
+            new_word = new_word + ending
 
-        # have access self which is the instance object
-    
-        # input 'happy'
-
-        # output appyhay
+            new_word.downcase!
+        else 
+            new_word = word + "way"
+            new_word.downcase!
+        end
 
         # return string of the word changed to Pig Latin
-    
+        new_word
     end
 
     def ultimate_method
         array_of_words = self.create_array_of_words
-        array_of_words.each do |word|
-            puts word
+        arr = array_of_words.map do |word|
+            self.convert(word)
         end
-
+        arr.join(" ")
     end
 
 
